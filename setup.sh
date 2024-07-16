f6934d87efb247d98e8425bd9741bec6c9cd01dd
Kazumi Momoki <kazumi.momoki@oracle.com>
2024-07-17 00:27:11 +0900

    copying service file, other changes

--- setup.sh
+++ setup.sh
@@ -3,3 +3,13 @@ echo "Setting up VCN server..."
 
 yum groupinstall "Server with GUI" -y
 yum install tigervnc-server -y
+
+cp /usr/lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@.service
+
+# set up oracle user
+bash setup_user.sh
+
+# vncserver service file
+cp vncserver@.service /etc/systemd/system/vncserver@.service
+systemctl daemon-reload
+
---
+++ setup_user.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+useradd oracle
+/usr/sbin/groupadd oinstall
+/usr/sbin/usermod -g oinstall oracle
