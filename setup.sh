392d28ced5ba4d0fd49017772d49870b056b94de
Kazumi Momoki <150307019+omomoki@users.noreply.github.com>
2024-09-03 20:54:19 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,13 @@ echo "Installing TigerVNC..."
 sudo dnf install -y tigervnc-server tigervnc-server-module
 
 echo "Setting VNC password. Note: this is for the currently logged in user."
-vncpasswd
+password="oracle"
+printf "$password\n$password\n\n" | vncpasswd
+echo "password set: $password"
 
 # start vncserver on startup
 sudo systemctl enable --now vncserver@:1.service
 
 vncserver
+
+echo "Setup complete! Now you can try to connect. You may need to setup port-forwarding on your local machine. If so, see the readme."
